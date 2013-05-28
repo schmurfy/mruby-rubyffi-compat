765faf0f300473190a30006350d8b6aa7242aa89
ppibburr <tulnor@linuxwaves.com>
2013-05-28 12:35:09 -0400

    Remove Array#flatten; Remove Array#find_all_indices
    Core gems provide Array#flatten now.
    Array#find_all_indices not used in this code base

--- mrblib/ffi.rb
+++ mrblib/ffi.rb
@@ -1,24 +1,3 @@
-
-class Array
-  def find_all_indices a=self,&b
-    o = []
-    a.each_with_index do |q,i|
-      if b.call(q)
-        o << i
-      end
-    end
-    return o
-  end
-  
-  def flatten
-    a=[]
-    each do |q|
-      a.push(*q)
-    end
-    return a
-  end  
-end
-
 module FFI
   def self.errno
     return CFunc.errno
