602714ee1f618666fed0293ff3c699da7f384dd1
ppibburr <tulnor@linuxwaves.com>
2013-05-30 13:19:02 -0300

    match proposed mruby-cfunc layout of Array members

--- mrblib/lib/struct.rb
+++ mrblib/lib/struct.rb
@@ -2,22 +2,6 @@ module FFI
   class Struct < CFunc::Struct
     alias_method :pointer, :addr
     
-    def array_members
-      self.class.instance_variable_get("@array_members")
-    end    
-    
-    def [] k
-      if (map = (array_members || {})[k])
-  t,s = map[0],map[1]
-	ptr = super(k)
-	CFunc::CArray(FFI.find_type(t)).refer(ptr.addr)
-      else
-	super
-      end
-    end  
-   
-    
-    
     def self.members
       elements.map{|e| e[1] }
     end
@@ -36,7 +20,7 @@ module FFI
           d << q.shift
         end
 
-        d[1] = FFI.find_type(d[1]) unless d[1].respond_to?(:"is_struct?")
+        d[1] = FFI.find_type(d[1]) unless d[1].respond_to?(:"is_struct?") || d[1].is_a?(CFunc::CArray)
         b.push(*d.reverse)
         d=[]
       end
@@ -44,20 +28,7 @@ module FFI
     end
   
     def self.layout *o
-      l = nil
-      n = o.map do |q|
-	if q.is_a?(Array)
-	  t = q[0]
-	  s = q[1]
-	  (@array_members ||= {})[l] = q
-	  next(:pointer)
-	else
-	  l = q
-	  next q
-	end
-      end
-      
-      define(*every(n,2))
+      define(*every(o,2))
     end
   end
   
