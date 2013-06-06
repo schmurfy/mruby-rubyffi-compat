a438d821b7148e7f8ff1a6bf708b94343db9d284
ppibburr <tulnor33@gmail.com>
2013-06-06 18:10:56 -0400

    improve MRI api compatbility

--- mrblib/lib/function.rb
+++ mrblib/lib/function.rb
@@ -1,50 +1,131 @@
-class Function
-  attr_reader :return_type
-
-  def initialize(lib, name, args, return_type)
-    @arguments = args
-    @lib = lib
-    @name = name
-    @return_type = return_type
+module FFI
+  # Describes a FFI::Function
+  class FunctionType
+    attr_accessor :param_types,:return_type,:address
+    def initialize rt,pta,addr
+      @param_types = pta
+      @return_type = rt
+      @address = addr
+    end
   end
-  
-  
-  def get_return_type
-    return @return_type.get_c_type
-  end
-  
-  attr_reader :arguments
+end
 
-  def call *o
-    raise ArgumentError.new("#{o.length} for #{arguments.length}") unless o.length == arguments.length
-
-    invoked = []
+module FFI
+  # Represents a Function that can be invoked
+  class Function
+    attr_accessor :function_type,:invoker
     
-    arguments.each_with_index do |a,i|
-      a.set o[i]
-      
-      ptr = a.for_invoke
-      
-      if a.value == nil
-        # do not wrap nil, pass it as is !
-        invoked << nil
-      else
-        invoked << ptr
+    # @param rt [Symbol] that can be resolved to a type that describes the return type.
+    # @param pta [Array] of Symbol's that can be resolved to a type that describe the parameter types.
+    # @param addr [Array] of [DynamicLibrary,String] that describes where to find the function.
+    def initialize rt,pta,addr
+      @function_type = FunctionType.new(rt,pta,addr)
+    end
+    
+    # Calls the function represented passing params.
+    #
+    # @return the result of calling the function.
+    def invoke *o,&b
+      @invoker ||= create_invoker()
+      invoker.invoke *o,&b
+    end
+    
+    alias :call :invoke
+    
+    # Binds function by defining a method on `where` to `name`.
+    #
+    # @param where where to bind the function.
+    # @param name [String,Symbol] of the method name. 
+    def attach where,name
+      this = self
+      where.singleton_class.define_method(name) do |*args, &b|  
+	this.invoke(*args, &b)
       end
+    end  
+    
+    # Creates a FFICompat::FunctionInvoker.
+    #
+    # @return [FFICompat::FunctionInvoker] that was created
+    def create_invoker()
+      ft = function_type
+      rt,pta,address = ft.return_type,ft.param_types,ft.address
+      
+      i=0
+      args = pta.map do |a|
+      
+	arg=Argument.new
+	arg[:index] = i
+	
+	i=i+1
+	
+	arg[:type] = a  
+
+	arg
+      end
+      
+      rett = Return.new
+      
+
+      rett[:type] = rt
+      
+      address[0].send(:get_symbol,address[1])
+      
+      @invoker = FFICompat::FunctionInvoker.new(address[0], address[1], args, rett)
+    end  
+  end
+end
+
+module FFICompat
+  # Represents a function suitable for using with CFunc
+  class FunctionInvoker
+    def initialize(lib, name, args, return_type)
+      @arguments = args
+      @lib = lib
+      @name = name
+      @return_type = return_type
+    end
+    
+    # @return the absolute type of the Return
+    def get_return_type
+      return @return_type.get_c_type
+    end
+    
+    attr_reader :arguments
+
+    # Invokes the function passing paramaters. 
+    #
+    # @return the result of calling the function
+    def call *o
+      raise ArgumentError.new("#{o.length} for #{arguments.length}") unless o.length == arguments.length
+
+      invoked = []
+      
+      arguments.each_with_index do |a,i|
+	a.set o[i]
+	
+	ptr = a.for_invoke
+	
+	if a.value == nil
+	  # do not wrap nil, pass it as is !
+	  invoked << nil
+	else
+	  invoked << ptr
+	end
+      end
+
+      # call the function
+      r = @lib.call(get_return_type,@name.to_s,*invoked)
+      
+      arguments.each do |a|
+	a.set nil
+      end
+      
+      return(@return_type.type == :void ? nil : @return_type.to_ruby(r))
     end
 
-    # call the function
-    r = @lib.call(get_return_type,@name.to_s,*invoked)
-    
-    arguments.each do |a|
-      a.set nil
+    # @see #call
+    def invoke(*args, &b)
+      call(*args, &b)
     end
-    
-    return(@return_type.type == :void ? nil : @return_type.to_ruby(r))
   end
-
-  def invoke(*args, &b)
-    call(*args, &b)
-  end
-  
 end
--- mrblib/lib/library.rb
+++ mrblib/lib/library.rb
@@ -1,80 +1,107 @@
-module FFI::Library
-  def ffi_lib(libnames)
-    libnames = [libnames] unless libnames.is_a?(Array)
-    libnames.each do |libname|
-      lib = FFICompat::DynamicLibrary.new(libname)
-      
-      if lib.exist? || (libnames[0] == :c)
-        @ffi_lib = lib
-        break
+module FFI
+  module Library
+    # @param [Array] names names of libraries to load
+    # @return [Array<FFICompat::DynamicLibrary>]
+    # @raise if a library cannot be opened
+    # Load native libraries.    
+    def ffi_lib(libnames)
+      @ffi_libs ||= []
+      libnames = [libnames] unless libnames.is_a?(Array)
+      libnames.each do |libname|
+	lib = FFICompat::DynamicLibrary.new(libname)
+	
+	if lib.exist? || (libnames[0] == :c)
+	  @ffi_libs << lib
+	end
       end
-    end
-        
-    unless @ffi_lib
-      raise "library not found: #{libnames}"
-    end
-  end
-  
-  @@callbacks = {}
-  def callback(name, arguments_type, result_type)
-    @@callbacks[name] = [ arguments_type.map{|t| FFI::TYPES[t]}, FFI::TYPES[result_type] ]
-  end
-  
-  def self.callbacks
-    @@callbacks
-  end
-  
-  @@typedefs = {}
-  def typedef(type_name, alias_name)
-    @@typedefs[alias_name] = type_name
-    FFI::TYPES[alias_name] = FFI.find_type(type_name)
-  end  
-  
-  def self.typedefs
-    @@typedefs
-  end
-  
-  @@enums = {}
-  def enum(enum_name, arr)
-    idx = 0
-    values = {}
-    
-    pos = 0
-    loop do
-      name = arr[pos]
-      value = arr[pos+1]
-      
-      if value.is_a?(Integer)
-        idx = value
-        pos += 2
-      else
-        pos += 1
+	  
+      if @ffi_libs.empty?
+	raise "library not found: #{libnames}"
       end
       
-      values[name] = idx
-      idx += 1
+      @ffi_libs
+    end
+    
+    @@callbacks = {}
+    def callback(name, arguments_type, result_type)
+      @@callbacks[name] = [ arguments_type.map{|t| FFI::TYPES[t]}, FFI::TYPES[result_type] ]
+    end
+    
+    def self.callbacks
+      @@callbacks
+    end
+    
+
+    @@typedefs = {}
+    def typedef(type_name, alias_name)
+      @@typedefs[alias_name] = type_name
+      FFI::TYPES[alias_name] = FFI.find_type(type_name)
+    end  
+    
+    def self.typedefs
+      @@typedefs
+    end
+    
+    @@enums = {}
+    def enum(enum_name, arr)
+      idx = 0
+      values = {}
       
-      break if pos >= arr.size
+      pos = 0
+      loop do
+	name = arr[pos]
+	value = arr[pos+1]
+	
+	if value.is_a?(Integer)
+	  idx = value
+	  pos += 2
+	else
+	  pos += 1
+	end
+	
+	values[name] = idx
+	idx += 1
+	
+	break if pos >= arr.size
+      end
+      
+      @@enums[enum_name] = values
+      typedef(:int, enum_name)
+      
+      self
+    end
+
+    def self.enums
+      @@enums
+    end  
+    
+    # @example attach function without an explicit name
+    #   module Foo
+    #     extend FFI::Library
+    #     ffi_lib FFI::Library::LIBC
+    #     attach_function :malloc, [:size_t], :pointer
+    #   end
+    #   # now callable via Foo.malloc    
+    # Attach C function +func+ to this module.
+    #
+    #
+    # @param [#to_s] name name of ruby method to attach as
+    # @param [#to_s] func name of C function to attach
+    # @param [Array<Symbol>] args an array of types
+    # @param [Symbol] returns type of return value
+    #
+    # @return [FFI::Function]    
+    def attach_function(function_name, arguments_type, result_type)
+      lib = @ffi_libs.find do |l|
+	l.send(:get_symbol,function_name)
+      end
+      
+      f = Function.new(result_type, arguments_type,[lib,function_name])
+
+      f.attach(self,function_name)
+      
+      f
     end
     
-    @@enums[enum_name] = values
-    typedef(:int, enum_name)
-    
-    self
   end
-
-  def self.enums
-    @@enums
-  end  
-  
-  def attach_function(function_name, arguments_type, result_type)
-    f = @ffi_lib.create_function(function_name, arguments_type, result_type)
-
-    singleton_class.define_method(function_name) do |*args, &b|  
-      f.invoke(*args, &b)
-    end
-    
-    self
-  end
-  
 end
